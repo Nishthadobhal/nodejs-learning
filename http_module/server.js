629cbdd482d2b0fbd7e2407effea2883993af8d5
nishthadobhal <nishthadobhaal2005@gmail.com>
2025-08-03 19:22:51 +0530

    http

--- http_module/server.js
+++ http_module/server.js
@@ -2,7 +2,7 @@ const http= require('http');
 //create server
 const server=http.createServer((req,res)=>{
     if(req.url === "/"){
-        res.write("welcome");
+        res.write("welcome nishtha");
         res.end();
     }
 
