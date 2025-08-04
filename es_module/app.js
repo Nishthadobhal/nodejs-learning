2e79c5aab3b7b16484e385edcf8c1926402a068f
nishthadobhal <nishthadobhaal2005@gmail.com>
2025-08-04 19:12:03 +0530

    npm

---
+++ es_module/app.js
@@ -0,0 +1,8 @@
+// const add=require("./math")
+
+// const {add,sub}=require("./math");
+
+import mult from "./math.js";
+// console.log(add(5,10));
+// console.log(sub(5,10));
+console.log(mult(50,10));
